47faaa7363f85fe4538747312216d31a8db9c47b
Melf <cqc@melf.de>
2025-01-10 17:59:58 +0000

    fix ruff issues

--- python/hugr_qir/tests/test_guppy_examples.py
+++ python/hugr_qir/tests/test_guppy_examples.py
@@ -7,8 +7,8 @@ from hugr_qir._hugr_qir import cli
 GUPPY_EXAMPLES_DIR = Path(__file__).parent / "../../../guppy_examples/"
 
 
-def guppy_to_hugr_json_file(guppy_file, outfd) -> None:
-    subprocess.run(
+def guppy_to_hugr_json_file(guppy_file: str, outfd: bytes) -> None:
+    subprocess.run(  # noqa: S603
         [sys.executable, GUPPY_EXAMPLES_DIR / guppy_file],
         check=True,
         stdout=outfd,
@@ -16,14 +16,14 @@ def guppy_to_hugr_json_file(guppy_file, outfd) -> None:
     )
 
 
-def cli_on_guppy(guppy_file, tmp_path, *args) -> None:
+def cli_on_guppy(guppy_file: str, tmp_path: Path, *args: dict) -> None:
     guppy_file = Path(guppy_file)
     json_file = tmp_path / Path(f"{guppy_file.name}.json")
-    with open(json_file, "w") as f:
+    with Path.open(json_file, "w") as f:
         guppy_to_hugr_json_file(guppy_file, f)
     cli(str(json_file), *[str(arg) for arg in args])
 
 
-def test_guppy_planqc_q(tmp_path) -> None:
+def test_guppy_planqc_q(tmp_path: Path) -> None:
     out_file = tmp_path / "out.ll"
     cli_on_guppy("planqc_1.py", tmp_path, "-o", out_file)
