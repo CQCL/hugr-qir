3aebe82d89a0134b5b544bb1107a32f044f03e1b
Travis Thompson <travis.thompson@quantinuum.com>
2025-02-06 15:01:47 +0100

    Check generated qir with quantinuum-qircheck (expected failure for now)

--- python/hugr_qir/tests/test_guppy_examples.py
+++ python/hugr_qir/tests/test_guppy_examples.py
@@ -1,29 +1,38 @@
 import subprocess
+from typing import Any
+
+import pytest
 import sys
 from pathlib import Path
 
 from hugr_qir._hugr_qir import cli
+from quantinuum_qircheck import qircheck
 
 GUPPY_EXAMPLES_DIR = Path(__file__).parent / "../../../guppy_examples/"
 
 
-def guppy_to_hugr_json_file(guppy_file: str, outfd: bytes) -> None:
+def guppy_to_hugr_json_file(guppy_file: Path, outfd: bytes) -> None:
     subprocess.run(  # noqa: S603
-        [sys.executable, GUPPY_EXAMPLES_DIR / guppy_file],
+        [sys.executable, guppy_file],
         check=True,
         stdout=outfd,
         text=True,
     )
 
 
-def cli_on_guppy(guppy_file: str, tmp_path: Path, *args: dict) -> None:
+def cli_on_guppy(guppy_file: Path, tmp_path: Path, *args: Any) -> None:
     guppy_file = Path(guppy_file)
     json_file = tmp_path / Path(f"{guppy_file.name}.json")
     with Path.open(json_file, "w") as f:
         guppy_to_hugr_json_file(guppy_file, f)
     cli(str(json_file), *[str(arg) for arg in args])
 
-
-def test_guppy_planqc_q(tmp_path: Path) -> None:
+@pytest.mark.xfail(reason="hugr to qir conversion still a wip")
+def test_guppy_files(tmp_path: Path) -> None:
     out_file = tmp_path / "out.ll"
-    cli_on_guppy("planqc_1.py", tmp_path, "-o", out_file)
+    guppy_dir = Path(GUPPY_EXAMPLES_DIR)
+    for file in guppy_dir.glob('*.py'):
+        cli_on_guppy(file, tmp_path, "-o", out_file)
+        with Path.open(out_file) as f:
+            qir = f.read()
+        qircheck(qir)
