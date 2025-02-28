cb2469fd6be5e028e93795759f849d5d7ce18c16
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2025-02-28 15:53:25 +0000

    tests: add more guppy examples (#22)

---
+++ guppy_examples/quantum-classical-1.py
@@ -0,0 +1,25 @@
+from guppylang import guppy, quantum, qubit
+from guppylang.std.angles import angles
+from guppylang.std.builtins import result
+from guppylang.std.quantum import h, measure
+
+guppy.load(quantum)
+guppy.load(angles)
+
+
+@guppy
+def main() -> None:
+    q0 = qubit()
+    q1 = qubit()
+
+    h(q0)
+    h(q1)
+
+    b0 = measure(q0)
+    b1 = measure(q1)
+    b2 = b0 ^ b1
+
+    result("0", b2)
+
+if __name__ == "__main__":
+    print(guppy.get_module().compile().package.to_json())
