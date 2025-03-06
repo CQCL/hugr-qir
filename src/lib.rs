451ed6eb0d3a51f798a79710e221db28687153d0
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2025-03-06 16:03:09 +0000

    Add meta data to main function (#24)
    @trvto and I have tried to add the metadata.
    
    The number of qubits and results still need to be extracted from the
    hugr.
    
    Solves https://github.com/CQCL/hugr-qir/issues/18
    
    ---------
    
    Co-authored-by: Travis Thompson <travis.thompson@quantinuum.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,15 @@ use hugr::llvm::custom::CodegenExtsMap;
 use hugr::llvm::emit::{EmitHugr, Namer};
 use hugr::llvm::utils::fat::FatExt;
 use hugr::llvm::{inkwell, CodegenExtsBuilder};
-use hugr::Hugr;
+use hugr::{Hugr};
+use hugr_llvm::inkwell::attributes::AttributeLoc;
 use inkwell::context::Context;
 use inkwell::module::Module;
 use qir::{QirCodegenExtension, QirPreludeCodegen};
 use rotation::RotationCodegenExtension;
 use inkwell::passes::PassManager;
 use anyhow::anyhow;
+use hugr::HugrView;
 
 pub mod cli;
 pub mod qir;
@@ -106,18 +108,58 @@ impl CompileArgs {
         let extensions = self.codegen_extensions().into();
         let namer = Rc::new(Namer::new("__hugr__.", true));
         let module = context.create_module(self.module_name().as_ref());
-        let emit = EmitHugr::new(context, module, namer, extensions);
-        Ok(emit.emit_module(hugr.fat_root().unwrap())?.finish())
+        let emit = EmitHugr::new(context, module, namer.clone(), extensions);
+        let module = emit.emit_module(hugr.fat_root().unwrap())?.finish(); 
+        add_module_metadata(&namer, hugr, &module)?;
+        Ok(module)
     }
 
     pub fn compile<'c>(&self, hugr: &mut Hugr, context: &'c Context) -> Result<Module<'c>> {
         self.hugr_to_hugr(hugr)?;
-
         let module = self.hugr_to_llvm(hugr, context)?;
         self.optimize_module(&module)?;
         return Ok(module);
+    }}
+
+
+    pub fn find_entry_point(namer: &Namer, hugr: &impl HugrView) -> Result<String> {
+        let entry_point_node = {
+            let mains: Vec<_> = hugr
+                .nodes()
+                .filter(|&n| {
+                    hugr.get_optype(n)
+                        .as_func_defn()
+                        .is_some_and(|f| f.name == "main")
+                })
+                .collect();
+            match mains.as_slice() {
+                [] => Err(anyhow!("main function not found in HUGR"))?,
+                [x] => *x,
+                xs => Err(anyhow!("found {} main functions in HUGR", xs.len()))?,
+            }
+        };
+        Ok(namer.name_func("main", entry_point_node))
     }
-}
+
+    pub fn add_module_metadata(namer: &Namer, hugr: &impl HugrView, module: &Module) ->  Result<()> {
+        let attributes = [
+            module.get_context().create_string_attribute("entry_point", ""),
+            module.get_context().create_string_attribute("output_labeling_schema", ""),
+            module.get_context().create_string_attribute("qir_profiles", "custom"),
+            module.get_context().create_string_attribute("required_num_qubits", "20"),
+            module.get_context().create_string_attribute("required_num_results", "20"),
+            // see https://github.com/CQCL/hugr-qir/issues/27
+        ];
+        let entry_func_name = find_entry_point(&namer, hugr).unwrap();
+        let fn_value = module.get_function(&entry_func_name);
+        if fn_value == None {
+            return Err(anyhow!("expected main function: \"{}\" not found in HUGR", entry_func_name));
+        }
+        for attribute in attributes {
+            fn_value.unwrap().add_attribute(AttributeLoc::Function, attribute);
+        }
+        Ok(())
+    }
 
 #[cfg(test)]
 pub(crate) mod test;
