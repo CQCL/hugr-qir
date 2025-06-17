a4ab6bb670e00f8b84d867d69fd393c9f3a39280
Melf <cqc@melf.de>
2025-06-17 13:13:37 +0100

    clean up

--- python/hugr_qir/hugr_to_qir.py
+++ python/hugr_qir/hugr_to_qir.py
@@ -2,7 +2,7 @@ import tempfile
 from base64 import b64encode
 from pathlib import Path
 
-from hugr.package import PackagePointer
+from hugr.package import ModulePointer
 from llvmlite.binding import (  # type: ignore[import-untyped]
     create_context,
     parse_assembly,
@@ -12,7 +12,7 @@ from .cli import hugr_qir_impl
 
 
 def hugr_to_qir(
-    hugr: PackagePointer | bytes, *, validate_qir: bool = True, emit_text: bool = False
+    hugr: ModulePointer | bytes, *, validate_qir: bool = True, emit_text: bool = False
 ) -> str:
     """A function for converting hugr to qir (llvm bitcode)
 
@@ -33,7 +33,7 @@ def hugr_to_qir(
         if type(hugr) is bytes:
             hugr_bytes = hugr
         else:
-            assert type(hugr) is PackagePointer
+            assert type(hugr) is ModulePointer  # noqa: S101
             hugr_bytes = hugr.package.to_bytes()
 
         with Path.open(Path(temp_infile.name), "wb") as cli_input:
